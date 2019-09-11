af7074307863f9825a0f405c9240da062116b7a3
Sam Anderson <sam.anderson@joipolloi.com>
2019-09-11 18:51:18 +0100

    Refactor updateTaskStatus

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -16,6 +16,7 @@ import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { TaskStatus } from './task-status.enum';
 
 @Controller('tasks')
 export class TasksController {
@@ -40,16 +41,16 @@ export class TasksController {
     return this.tasksService.createTask(createTaskDto);
   }
 
-  //   @Patch('/:id/status')
-  //   updateTaskStatus(
-  //     @Param('id') id: string,
-  //     @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  //   ): Task {
-  //     return this.tasksService.updateTaskStatus(id, status);
-  //   }
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
 
-    @Delete('/:id')
-    deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
-      return this.tasksService.deleteTask(id);
-    }
+  @Delete('/:id')
+  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.tasksService.deleteTask(id);
+  }
 }
