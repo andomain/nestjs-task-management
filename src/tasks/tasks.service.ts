f09d4d73e3ddbdaee6e1171454aa3f43eb82959c
Sam Anderson <sam.anderson@joipolloi.com>
2019-09-12 15:41:14 +0100

    Refactor delete task to use user

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -32,7 +32,11 @@ export class TasksService {
     return this.taskRepository.createTask(createTaskDto, user);
   }
 
-  async updateTaskStatus(id: number, status: TaskStatus, user: User): Promise<Task> {
+  async updateTaskStatus(
+    id: number,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
     const task = await this.getTaskById(id, user);
     task.status = status;
     await task.save();
@@ -40,8 +44,8 @@ export class TasksService {
     return task;
   }
 
-  async deleteTask(id: number): Promise<void> {
-    const result = await this.taskRepository.delete(id);
+  async deleteTask(id: number, user: User): Promise<void> {
+    const result = await this.taskRepository.delete({ id, userId: user.id });
     if (result.affected === 0) {
       throw new NotFoundException(`Task with ID ${id} not found`);
     }
